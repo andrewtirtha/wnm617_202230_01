1a3453e1ad76a749fe562190176c8187678a2e27
Andrew Tirtha <andrewtirtha@Andrews-MacBook.local>
2022-02-15 10:23:25 +0700

    added css files and profile html

---
+++ tirtha.andrew/parts/meta.php
@@ -0,0 +1,15 @@
+<base href="/aau/wnm608/m11/">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="css/styleguide.css">
+<link rel="stylesheet" type="text/css" href="css/gridsystem.css">
+<link rel="stylesheet" type="text/css" href="css/theme.css">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet">
+
+
+<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+<script src="js/products.js"></script>
---
+++ tirtha.andrew/parts/navbar.php
@@ -0,0 +1,40 @@
+
+<header>
+		<nav class="nav stretch">
+			<ul>
+				<li><a href="profile.php">Profile</a></li>
+				<li><a href="work.php">Work</a></li>
+			</ul>
+		</nav>
+
+
+</header>
+
+<!--
+
+
+<header class="navbar">
+	<div class="container display-flex flex-align-center">
+		<div class="flex-none">
+			<img class="logo" src="img/website/logo.png">
+		</div>
+		<div class="flex-stretch"></div>
+
+		<nav class="nav flex-none">
+			<ul class="display-flex">
+				<li><a href="home.php">Home</a></li>
+				<li><a href="about.php">About</a></li>
+				<li><a href="product_list.php">Store</a></li>
+				<li><a href="product_cart.php">
+					<span>Cart</span>
+					<span class="badge"><?= makeCartBadge() ?></span>
+				</a></li>
+			</ul>
+		</nav>
+
+	</div>
+
+</header>
+
+
+-->
