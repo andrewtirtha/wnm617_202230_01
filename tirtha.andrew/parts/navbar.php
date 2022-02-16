5042d5547c2593e792ea929d68b5874b3a4caeeb
Andrew Tirtha <andrewtirtha@Andrews-MacBook.local>
2022-02-16 09:02:33 +0700

    ade bio and also style adjustments

--- tirtha.andrew/parts/navbar.php
+++ tirtha.andrew/parts/navbar.php
@@ -1,8 +1,8 @@
 
 <header>
-		<nav class="nav-pills" margin-left="50px">
-				<ul><a href="profile.php">01: Profile</a></ul>
-				<ul><a href="work.php">02: Work</a></ul>
+		<nav class="nav-pills" style="margin-left: 50px; margin-top: 20px;">
+				<a href="profile.php" style="margin-left: 50px;">01: Profile</a>
+				<a href="work.php" style="margin-left: 50px;">02: Work</a>
 		</nav>
 
 
--- tirtha.andrew/profile.php
+++ tirtha.andrew/profile.php
@@ -10,16 +10,33 @@ include_once "php/functions.php";
 </head>
 <body>
 	<?php include "parts/navbar.php"?>
-	<br>
 	<div class="container">
-		<div>
-			<h1>Andrew Tirtha</h1>
-			<h3>UI/UX and Product Designer.</h3>
+		<div class="main-pic"><img src="img/andrew.JPG" width="300"></div>
+		<div class="main-text">
+			<h1 font-style: italic;>Hi! 
+			<h2>I'm Andrew Tirtha,</h2>
+			<h3 margin-top="10px">UI/UX and Product Designer.</h3>
+			<h5>I was born and raised in Jakarta, Indonesia, but I am currently based in San Francisco, CA. I'm pursuing my master's degree in Interaction and UI/UX Design at the Academy of Art University. I love finding creative(and aesthetic!) solutions to any design problems I come across. </h5>
 		</div>
+	</div>
+	<div class="container-section">
+		<h3 style="color: #000000;">Projects </h4>
+		<h5>Below are some of my projects. Come take a look! </h5>
+		<div class="grid-container">
+ 		    <div class="grid-item">1</div>
+		    <div class="grid-item">2</div>
+  			<div class="grid-item">3</div>
+  			<div class="grid-item">4</div>
+  			<div class="grid-item">5</div>
+  			<div class="grid-item">6</div>
+		</div>		
 	</div>
 
 
 
-		<footer class="footer">&#169; 2022 andrewtirtha.com, All Rights Reserved. Website by Andrew Tirtha</footer>
+
+
+
+	<!-- <footer class="footer">&#169; 2022 andrewtirtha.com, All Rights Reserved. Website by Andrew Tirtha</footer> -->
 </body>
 </html>
